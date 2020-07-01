f92591fefcb2b77eea7ae1582918e21371e45416
ho-hyeong <67277531+ho-hyeong@users.noreply.github.com>
2020-07-01 19:30:32 +0900

    done

--- app.js
+++ app.js
@@ -3,14 +3,21 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
+
+const INITIAL_COLOR = "#2c2c2c";
+const CANVAS_SIZE = 500;
 
 
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
 
-canvas.width = 500;
-canvas.height = 500;
-
-ctx.strokeStyle = "#2c2c2c";
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+ctx.strokeStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
+
+ctx.fillStyle = INITIAL_COLOR;
 
 let painting = false;
 let filling = false;
@@ -38,6 +45,7 @@ function onMouseMove(event){
 function handleColorClick(event){
 	const color = event.target.style.backgroundColor;
 	ctx.strokeStyle = color;
+	ctx.fillStyle = color;
 }
 
 function handleRangeChange(event){
@@ -55,12 +63,31 @@ function handleModeClick(){
 	}
 }
 
+function handleCanvasClick(){
+	if(filling){
+		ctx.fillRect(0,0,CANVAS_SIZE,CANVAS_SIZE);
+	}
+}
+
+function handleLeCM(event){
+	event.preventDefault();
+}
+
+function handleSaveClick(){
+	const image = canvas.toDataURL();
+	const link = document.createElement("a");
+	link.href = image;
+	link.download = "PaintJS[🎨]";
+	link.click();
+}
+
 if(canvas){
 	canvas.addEventListener("mousemove", onMouseMove);
 	canvas.addEventListener("mousedown", startPainting);
 	canvas.addEventListener("mouseup", stopPainting);
 	canvas.addEventListener("mouseleave", stopPainting);
-
+	canvas.addEventListener("click", handleCanvasClick);
+	canvas.addEventListener("contextmenu",handleLeCM);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
@@ -72,3 +99,8 @@ if(range){
 if(mode){
 	mode.addEventListener("click", handleModeClick);
 }
+
+if(saveBtn){
+	saveBtn.addEventListener("click", handleSaveClick);
+}
+
